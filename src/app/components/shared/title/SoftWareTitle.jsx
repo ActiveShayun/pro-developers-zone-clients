13368818d8ff6127d5e6cd2a01482ce4b42ade32
ActiveShayun <wevdeveloper0477gmail.com>
2025-08-12 23:35:19 +0600

    softeare grid  section done

--- src/app/components/shared/title/SoftWareTitle.jsx
+++ src/app/components/shared/title/SoftWareTitle.jsx
@@ -2,23 +2,17 @@ import React from 'react';
 import { SiGooglegemini } from "react-icons/si";
 import { HiMiniArrowTrendingUp } from "react-icons/hi2"
 
-const SoftWareTitle = ({ firstLine, greyText, lastText, secondLine, lastLine }) => {
+const SoftWareTitle = () => {
     return (
-        <h1 className="text-4xl lg:text-6xl font-medium space-y-2 lg:text-center text-left">
-            <span className="lg:flex flex-wrap items-center lg:justify-center justify-start gap-2">
-                <img className='h-12 w-12' src={'/gemini.png'} alt="" />
-                <span>{firstLine}</span>
-                <span className="text-gray-500 lg:mx-3">{greyText}</span>
-                {lastText}
-            </span>
-            <span className="lg:block lg:mt-3">{secondLine}</span>
-            <span className="text-gray-500 lg:flex items-center lg:justify-center justify-start mt-2">
-                {lastLine}
-                <HiMiniArrowTrendingUp className="text-3xl lg:text-4xl -rotate-90 relative top-1 lg:top-2 ml-1" />
-            </span>
+        <h2 className='text-4xl lg:text-6xl font-medium lg:text-center text-left'>
+            <img className='kg:h-12 h-10 w-10 inline-block' src={'/ai.PNG'} alt="" />
+            At Software Chamber,
+            <span className='text-gray-500 ml-2'>we specialize </span>
+            in turning complex challenges into elegant
+            <span className='text-gray-500 ml-3 lg:block'>digital solutions</span>
+            <HiMiniArrowTrendingUp className="inline-block text-3xl lg:text-4xl -rotate-90 relative  lg:top-2 ml-1" />
 
-        </h1>
-
+        </h2>
     );
 };
 
--- src/app/components/softwareChamber/SoftWareChamber.jsx
+++ src/app/components/softwareChamber/SoftWareChamber.jsx
@@ -7,40 +7,77 @@ const SoftWareChamber = () => {
     return (
         <div className='bg-white lg:mt-[108px] mt-16 lg:py-20 py-10 px-4 lg:px-8'>
             <div>
-                <SoftWareTitle
-                    firstLine={' At Software Chamber,'}
-                    greyText={'we specialize'}
-                    lastText={'in'}
-                    secondLine={'turning complex challenges into elegant'}
-                    lastLine={'digital solutions'}
-                />
+                <SoftWareTitle />
             </div>
-            <section className='grid grid-cols-1 lg:grid-cols-8 gap-4 w-full h-[500px] mt-4 lg:mt-20'>
+            <section className='grid grid-cols-1 lg:grid-cols-10 gap-4 w-full h-[500px] mt-4 lg:mt-20'>
                 {/* col 1 */}
                 <div className='flex flex-col justify-between col-span-2'>
                     <div>
-                        <span className='text-6xl block mb-3'>20+</span>
+                        <span className='text-5xl block mb-3'>20+</span>
                         <span className='text-3xl text-gray-500'>Tech Partners</span>
                     </div>
                     <div>
-                        <span className='text-6xl block mb-3'>150+</span>
+                        <span className='text-5xl block mb-3'>150+</span>
                         <span className='text-3xl text-gray-500'>Project Completed</span>
                     </div>
                     <div>
-                        <span className='text-6xl block mb-3'>1K+</span>
+                        <span className='text-5xl block mb-3'>1K+</span>
                         <span className='text-3xl text-gray-500'>Satisfied Customers</span>
                     </div>
                 </div>
-                {/* col 1 */}
-                <div className='col-span-3 relative h-full w-full group overflow-hidden'>
-                    <img 
-                    className='h-full object-cover rounded-xl group-hover:scale-120 transition transform duration-600 overflow-hidden'
-                     src={'/business.jpg'} alt="" />
+                {/* col 2 */}
+                <div className='col-span-5 relative h-full w-full group overflow-hidden'>
+                    <img
+                        className='h-full object-cover rounded-xl group-hover:scale-120 transition transform duration-600 overflow-hidden'
+                        src={'/business.jpg'} alt="" />
                     <div className='h-full w-full absolute top-0 left-0 bg-black rounded-xl opacity-40'></div>
                 </div>
                 {/* col 3 */}
-                <div className='col-span-3'>
-                    <img src={'/business.jpg'} alt="" />
+                <div className='col-span-5 flex flex-col justify-between'>
+                    <div className='grid grid-cols-6 items-start gap-4'>
+                        <div className='col-span-1 bg-[#27B4A3] w-[70px] h-[70px] rounded-full flex items-center justify-center'>
+                            <p className='bg-white w-2 h-2 p-3 flex items-center justify-center text-black rounded-full'>
+                                1
+                            </p>
+                        </div>
+                        <div className='col-span-5'>
+                            <h5 className='text-3xl'>Your Team in the cloud</h5>
+                            <p>We Hire Best and brightest Talents to build world class technology</p>
+                        </div>
+                    </div>
+                    <div className='grid grid-cols-6 items-start gap-4'>
+                        <div className='col-span-1 bg-[#27B4A3] w-[70px] h-[70px] rounded-full flex items-center justify-center'>
+                            <p className='bg-white w-2 h-2 p-3 flex items-center justify-center text-black rounded-full'>
+                                1
+                            </p>
+                        </div>
+                        <div className='col-span-5'>
+                            <h5 className='text-4xl'>Grow more with less</h5>
+                            <p>We have a competitive pricing structure</p>
+                        </div>
+                    </div>
+                    <div className='grid grid-cols-6 items-start gap-4'>
+                        <div className='col-span-1 bg-[#27B4A3] w-[70px] h-[70px] rounded-full flex items-center justify-center'>
+                            <p className='bg-white w-2 h-2 p-3 flex items-center justify-center text-black rounded-full'>
+                                1
+                            </p>
+                        </div>
+                        <div className='col-span-5'>
+                            <h5 className='text-4xl'>Weekly sprint and reviews</h5>
+                            <p>Our fast review and feedback loop is designed to keep in you in the driver's seat</p>
+                        </div>
+                    </div>
+                    <div className='grid grid-cols-6 items-start'>
+                        <div className='col-span-1 bg-[#27B4A3] w-[70px] h-[70px] rounded-full flex items-center justify-center'>
+                            <p className='bg-white w-2 h-2 p-3 flex items-center justify-center text-black rounded-full'>
+                                1
+                            </p>
+                        </div>
+                        <div  className='col-span-5'>
+                            <h5 className='text-4xl'>Communication First</h5>
+                            <p>We use modem tools and services to collaborate</p>
+                        </div>
+                    </div>
                 </div>
             </section>
         </div>
@@ -49,5 +86,3 @@ const SoftWareChamber = () => {
 
 export default SoftWareChamber;
 
-//At Software Chamber,
-// we specialize in 
